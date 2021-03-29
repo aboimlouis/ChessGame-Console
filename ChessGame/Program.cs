039b6781c89af2d6193ee32089a33b8de5629704
Louis Aboim <aboimlouis@gmail.com>
2021-03-29 20:01:52 -0300

    Added the position class

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ChessGame.Board;
 
 namespace ChessGame
 {
@@ -6,7 +7,7 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position P;
         }
     }
 }
